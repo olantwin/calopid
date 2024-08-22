cb5c71a90180c86bd2bf377e19c4ab700aaf9952
Oliver Lantwin <oliver.lantwin@cern.ch>
2024-08-22 14:03:04 +0200

    Update config.sh for 2024/08

--- config.sh
+++ config.sh
@@ -1,11 +1,13 @@
+TAG=2024/08
 ADVSNDBUILD_DIR=/afs/cern.ch/user/o/olantwin/SND
 source /cvmfs/sndlhc.cern.ch/SNDLHC-2024/June25/setUp.sh
 source $ADVSNDBUILD_DIR/nusim_automation/env_cleanup.sh
-source $ADVSNDBUILD_DIR/advsnd_minimal_240724.sh
+source $ADVSNDBUILD_DIR/advsnd_minimal_240821.sh
 NEUTRINO=12
-EVENTGENLIST=CCDIS
-OUTPUT_PREFIX=2024/07/nu$NEUTRINO/$EVENTGENLIST
+EVENTGENLIST=Default
+OUTPUT_PREFIX=$TAG/nu$NEUTRINO/$EVENTGENLIST
 EOSSERVER=root://eospublic.cern.ch/
 export FAIRSHIP_ROOT=$ADVSNDSW_ROOT
 export FAIRSHIP=$FAIRSHIP_ROOT
 export SNDSW_ROOT=$ADVSNDSW_ROOT
+GEOFILE="/afs/cern.ch/user/o/olantwin/SND/nusim_automation/geofile.advsnd_minimal_v4.gdml"
