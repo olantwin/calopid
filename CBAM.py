bb7a9304d4326bf3a24b1bb4332fb466406017b5
grassim <m.grassi15@studenti.unibg.it>
2025-02-12 11:57:30 +0100

    Deserialization and documentation added

--- CBAM.py
+++ CBAM.py
@@ -4,13 +4,41 @@ from tensorflow.keras.layers import (Add, Concatenate, Conv1D, Dense,
                                      GlobalAveragePooling1D,
                                      GlobalMaxPooling1D, Multiply, Reshape)
 
-
 class CBAM(layers.Layer):
-    def __init__(self, ratio=8, name=None):
-        super(CBAM, self).__init__(name=name)
+    """
+    Convolutional Block Attention Module (CBAM) layer.
+
+    CBAM sequentially applies channel and spatial attention mechanisms to the input tensor.
+    This enhances feature representations by focusing on the most informative parts of the input.
+
+    Attributes:
+        ratio (int): Reduction ratio for channel attention.
+
+    Methods:
+        call(input_tensor): Applies the CBAM mechanism to the input tensor.
+        get_config(): Returns the configuration of the layer for serialization.
+        from_config(config): Creates a layer instance from its configuration.
+    """
+
+    def __init__(self, ratio=8, name=None, **kwargs):
+        """
+        Initializes the CBAM layer.
+
+        Args:
+            ratio (int): Reduction ratio for channel attention. Default is 8.
+            name (str, optional): Name of the layer. Default is None.
+            **kwargs: Additional keyword arguments for the parent Layer class.
+        """
+        super(CBAM, self).__init__(name=name, **kwargs)
         self.ratio = ratio
 
     def build(self, input_shape):
+        """
+        Builds the layer by initializing its weights and submodules.
+
+        Args:
+            input_shape (TensorShape): Shape of the input tensor.
+        """
         filters = input_shape[-1]
 
         self.global_avg_pool = GlobalAveragePooling1D()
@@ -22,6 +50,16 @@ class CBAM(layers.Layer):
         self.conv = Conv1D(1, kernel_size=7, padding="same", activation="sigmoid")
 
     def call(self, input_tensor):
+        """
+        Applies the CBAM mechanism to the input tensor.
+
+        Args:
+            input_tensor (Tensor): Input tensor to the layer.
+
+        Returns:
+            Tensor: Output tensor with enhanced feature representations.
+        """
+        # Channel attention
         avg_pool = self.global_avg_pool(input_tensor)
         max_pool = self.global_max_pool(input_tensor)
 
@@ -36,6 +74,7 @@ class CBAM(layers.Layer):
 
         x = Multiply()([input_tensor, channel_attention])
 
+        # Spatial attention
         avg_pool = tf.reduce_mean(x, axis=-1, keepdims=True)
         max_pool = tf.reduce_max(x, axis=-1, keepdims=True)
 
@@ -45,3 +84,28 @@ class CBAM(layers.Layer):
         x = Multiply()([x, spatial_attention])
 
         return x
+
+    def get_config(self):
+        """
+        Returns the configuration of the CBAM layer for serialization.
+
+        Returns:
+            dict: Configuration dictionary of the layer.
+        """
+        config = super(CBAM, self).get_config()
+        config.update({"ratio": self.ratio})
+        return config
+
+    @classmethod
+    def from_config(cls, config):
+        """
+        Creates a CBAM layer instance from its configuration.
+
+        Args:
+            config (dict): Configuration dictionary.
+
+        Returns:
+            CBAM: A new CBAM layer instance.
+        """
+        return cls(**config)
+
