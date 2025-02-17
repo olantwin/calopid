823304041e0d324909143e9a2f4c03334df838ea
Oliver Lantwin <oliver.lantwin@cern.ch>
2025-02-17 15:37:34 +0100

    chore(pre-commit): add bashate

--- imager.sh
+++ imager.sh
@@ -3,7 +3,7 @@ set -o errexit -o pipefail -o noclobber
 
 # Set up SND environment
 WORKDIR=/afs/cern.ch/work/o/olantwin/advsnd/calopid
-source $WORKDIR/config.sh
+source "${WORKDIR}"/config.sh
 
 set -o nounset
 
@@ -11,25 +11,26 @@ ProcId=$1
 LSB_JOBINDEX=$((ProcId+1))
 
 INPUTFILE=sndLHC.Genie-TGeant4_dig.root
-IMAGEFILE=images_1000_$LSB_JOBINDEX.npy
-FEATUREFILE=features_CNN_1000_$LSB_JOBINDEX.csv
+IMAGEFILE=images_1000_"${LSB_JOBINDEX}".npy
+FEATUREFILE=features_CNN_1000_"${LSB_JOBINDEX}".csv
 
-OUTPUTDIR=/eos/experiment/sndlhc/users/olantwin/advsnd/$OUTPUT_PREFIX/
+OUTPUTDIR=/eos/experiment/sndlhc/users/olantwin/advsnd/"${OUTPUT_PREFIX}"/
 
 set -x
 
-if xrdfs $EOSSERVER stat $OUTPUTDIR/$IMAGEFILE && xrdfs $EOSSERVER stat $OUTPUTDIR/$FEATUREFILE; then
-	echo "Target exists, nothing to do."
-	exit 0
+if xrdfs "${EOSSERVER}" stat "${OUTPUTDIR}"/"${IMAGEFILE}" && \
+    xrdfs "${EOSSERVER}" stat "${OUTPUTDIR}"/"${FEATUREFILE}"; then
+    echo "Target exists, nothing to do."
+    exit 0
 fi
 
 INDICES=$(seq $(( ProcId * 10 + 1 )) $(( ProcId * 10 + 10 )))
-INPUTFILES=$(for i in $INDICES; do echo $OUTPUTDIR/"$i"/$INPUTFILE; done)
+INPUTFILES=$(for i in ${INDICES}; do echo "${OUTPUTDIR}"/"${i}"/"${INPUTFILE}"; done)
 
-# shellcheck disable=SC2086
-python $WORKDIR/image_creator.py -j 4 --inputfiles $INPUTFILES
-mv images_1000.npy $IMAGEFILE
-mv features_CNN_1000.csv $FEATUREFILE
+# shellcheck disable=SC2086,SC2250
+python "${WORKDIR}"/image_creator.py -j 4 --inputfiles $INPUTFILES
+mv images_1000.npy "${IMAGEFILE}"
+mv features_CNN_1000.csv "${FEATUREFILE}"
 
-xrdcp $IMAGEFILE $EOSSERVER/$OUTPUTDIR/$IMAGEFILE
-xrdcp $FEATUREFILE $EOSSERVER/$OUTPUTDIR/$FEATUREFILE
+xrdcp "${IMAGEFILE}" "${EOSSERVER}"/"${OUTPUTDIR}"/"${IMAGEFILE}"
+xrdcp "${FEATUREFILE}" "${EOSSERVER}"/"${OUTPUTDIR}"/"${FEATUREFILE}"
--- ntupler.sh
+++ ntupler.sh
@@ -3,7 +3,7 @@ set -o errexit -o pipefail -o noclobber
 
 # Set up SND environment
 WORKDIR=/afs/cern.ch/work/o/olantwin/advsnd/calopid
-source $WORKDIR/config.sh
+source "${WORKDIR}"/config.sh
 
 set -o nounset
 
@@ -11,22 +11,22 @@ ProcId=$1
 LSB_JOBINDEX=$((ProcId+1))
 
 INPUTFILE=sndLHC.Genie-TGeant4_dig.root
-FEATUREFILE=features_1000_$LSB_JOBINDEX.csv
+FEATUREFILE=features_1000_"${LSB_JOBINDEX}".csv
 
-OUTPUTDIR=/eos/experiment/sndlhc/users/olantwin/advsnd/$OUTPUT_PREFIX/
+OUTPUTDIR=/eos/experiment/sndlhc/users/olantwin/advsnd/"${OUTPUT_PREFIX}"/
 
 set -x
 
-if xrdfs $EOSSERVER stat $OUTPUTDIR/$FEATUREFILE; then
-	echo "Target exists, nothing to do."
-	exit 0
+if xrdfs "${EOSSERVER}" stat "${OUTPUTDIR}"/"${FEATUREFILE}"; then
+    echo "Target exists, nothing to do."
+    exit 0
 fi
 
 INDICES=$(seq $(( ProcId * 10 + 1 )) $(( ProcId * 10 + 10 )))
-INPUTFILES=$(for i in $INDICES; do echo $OUTPUTDIR/"$i"/$INPUTFILE; done)
+INPUTFILES=$(for i in ${INDICES}; do echo "${OUTPUTDIR}"/"${i}"/"${INPUTFILE}"; done)
 
-# shellcheck disable=SC2086
-python $WORKDIR/ntuple_creator.py -j 4 --inputfiles $INPUTFILES
-mv features_1000.csv $FEATUREFILE
+# shellcheck disable=SC2086,SC2250
+python "${WORKDIR}"/ntuple_creator.py -j 4 --inputfiles $INPUTFILES
+mv features_1000.csv "${FEATUREFILE}"
 
-xrdcp $FEATUREFILE $EOSSERVER/$OUTPUTDIR/$FEATUREFILE
+xrdcp "${FEATUREFILE}" "${EOSSERVER}"/"${OUTPUTDIR}"/"${FEATUREFILE}"
